5d61bd4bf60bfe9080c3a5a9b74579d4ccf66e22
6310110356 <6310110356@psu.ac.th>
2022-08-27 21:05:34 +0700

    part 6-2

--- components/Weather.js
+++ components/Weather.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, ImageBackground, StyleSheet } from 'react-native'
 import Forecast from './Forecast';
 import Constants from 'expo-constants';
+import DateTime from './DateTime';
 
 export default function Weather(props) {
     useEffect(() => {
@@ -30,14 +31,26 @@ export default function Weather(props) {
     })
 
     return (
-        <View>
-            <ImageBackground source={require('../screen.jpg')} style={styles.backdrop}>
-                <View style={styles.highlight}>
-                    <Text style={styles.titleText}>Zip code is {props.zipCode}.</Text>
-                    <Forecast {...forecastInfo}/>
+        <ImageBackground source={require('../screen.jpg')} style={styles.backdrop}>
+        <View style={styles.highlight}>
+            <View>
+                
+                <View style={styles.container}>
+                    <Text style={styles.place}>{props.place}</Text>
                 </View>
-            </ImageBackground>
+                <View>
+                    <DateTime />
+                </View>
+                <View >
+                    <Text>{props.line}</Text>
+                    <Text style={styles.titleText}>Zip code is {props.zipCode}.</Text>
+                </View>
+            </View>
+            <View>
+                <Forecast {...forecastInfo}/>
+            </View>
         </View>
+        </ImageBackground>
     );
 }
 
@@ -59,6 +72,35 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: "bold",
         color: 'white',
+        textAlign: 'center',
+        fontWeight: '300'
+    },
+
+    section: {
+        flex: 1.5,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 25
+    },
+
+    place: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: '300',
+        textAlign: 'center'
+    },
+
+    container: {
+        backgroundColor: "#18181b99",
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 10
+    },
+
+    am: {
+        fontSize: 30,
+        color: 'white',
+        fontWeight: '300',
         textAlign: 'center'
     }
-});
+}); 
